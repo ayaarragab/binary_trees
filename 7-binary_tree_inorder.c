76a5a01332de245090958817e1d212a276f5290a
ayaarragab <ayarsk4343@gmail.com>
2023-10-31 23:58:57 +0200

    7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - a function that goes through
+ * binary_tree_inorder - a function that goes through
  * a binary tree using pre-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node.
@@ -13,6 +13,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!func)
 		return;
 	binary_tree_inorder(tree->left, func);
-    func(tree->n);
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 }

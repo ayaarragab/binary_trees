1f15d8d5ced166dd73cfcf8301ee0be43cfa2bc9
d12o6aa <doaakarem798@gmail.com>
2023-11-01 04:01:37 +0200

    again

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,26 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l = 0;
-    size_t r = 0;
+	size_t l = 0;
+	size_t r = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        if (tree)
-        {
-            if (tree->left)
-                l = 1 + binary_tree_height(tree->left);
-            else
-                l = 0;
-            if (tree->right)
-                l = 1 + binary_tree_height(tree->right);
-            else
-                l = 0;
-        }
-        return ((l > r) ? l : r);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+	{
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	}
+		return ((l > r) ? l : r);
+	}
 }
